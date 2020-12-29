8286168a71c57b80c26f3b8b776790ce8ace930f
yashsarjekar <yashsarjekar007@gmail.com>
2020-12-29 18:03:09 +0530

    second commit

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7r7^wti1b($w#p^x^9m4(1tu)at(xmyd(2s3^j63vi93a90il_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
